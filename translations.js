7875b09f437078ee040c7cfff2e4a771f97cc914
kbekmemetev <kbekmemetev@gmail.com>
2023-01-15 13:19:43 +0500

    added some features

---
+++ translations.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "Название вакансии",
+    description: "Описание",
+    key_skills: "Ключевые навыки",
+    experience_id: "Требуемый опыт",
+    premium: "Премиум вакансия",
+    employer_name: "Имя работодателя",
+    salary_from: "Минимальная зарплата",
+    salary_to: "Максимальная зарплата",
+    salary_gross: "Зарплата указана до вычета налогов",
+    salary_currency: "Валюта зарплаты",
+    area_name: "Регион",
+    published_at: "Дата публикации" 
+
+}
